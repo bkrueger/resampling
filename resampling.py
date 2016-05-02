12d1c7dd6ac3a48d5bf059b36c925f5ce5554332
Benedikt Krüger <benedikt.krueger@arcor.de>
2016-05-02 10:16:47 +0200

    Added support for specifying a function axis into the jackknife function, and created a private function to provide this support also to the other resampling methods.

--- resampling.py
+++ resampling.py
@@ -12,6 +12,36 @@ import numpy.random
 # How to apply a function over different axis
 # numpy.apply_over_axes(numpy.sum, a, [0,2])
 
+def __array_mean(a, indices, func_axis=None, dtype=None):
+    """Calculates the mean of an array using the given indices and the optional function axis.
+    
+    If no function axis is given, the function calculates the mean of the array at the given indices.
+    If a function axis is given, the function calculates the mean of the array at the given indices for each entry in the function axis seperatly and returns a tuple of means.
+
+    Parameters
+    ----------
+    a : array_like
+        Array containing numbers whose jackknife mean and error is desired. 
+        If `a` is not an array, a conversion is attempted.
+    indices : array_like
+        One-dimensional list of integer values between 0 and a.size for func_axis = None, or 0 and a.size / a.shape[func_axis] for func_axis != None.
+        The mean is calculated using this flat indices.
+    dtype : data-type, optional
+        Type to use in computing the jackknife mean and error. 
+        (For integer inputs, the default is `float64`; for floating point inputs, it is the same as the input dtype.)
+
+    Returns
+    -------
+    mean_value: tuple
+        Tuple containing the means of the dimensions of the function axis.
+        If func_axis == None, the tuple contains only one argument.
+    
+    """
+    if func_axis == None:
+        return numpy.mean(a.flat[indices], dtype=dtype)
+    else:
+        return tuple(numpy.mean(numpy.reshape(numpy.take(a, [j,], axis=func_axis), -1)[indices]) for j in range(a.shape[func_axis]))
+
 def identity(x):
     """
     Identity function used as default function in the resampling methods.
@@ -19,7 +49,7 @@ def identity(x):
     """
     return x
 
-def jackknife(a, func=identity, axis=None, dtype=None):
+def jackknife(a, func=identity, func_axis=None, dtype=None):
     r""" Compute the jackknife mean and error of an array (with :math:`N` values) with respect to a given function :math:`f`.
 
     Notes
@@ -56,8 +86,10 @@ def jackknife(a, func=identity, axis=None, dtype=None):
     >>> a = numpy.array([1.0, 2.0, 3.0, 4.0, 5.0])
     >>> jackknife(a)
     (3.0, 0.70710678118654757)
-    >>> jackknife(a, func=lambda x:x**2)
+    >>> jackknife(a, func=lambda x: x**2)
     (9.125, 4.247793544888923)
+    >>> b = numpy.array([[1.0, 2.0, 3.0, 4.0, 5.0], [1.0, 3.0, 5.0, 7.0, 9.0]])
+    >>> jackknife(b, func=lambda x,y: x*y, func_axis=0)
 
     References
     ----------
@@ -66,14 +98,21 @@ def jackknife(a, func=identity, axis=None, dtype=None):
 
     """
     # Jackknife for the whole array
-    n = len(a)
-    jackknife_values = numpy.fromiter((func(numpy.mean(a[range(0, i) + range(i+1, n)], dtype=dtype)) for i in range(n)), numpy.float)
+    if (func_axis == None):
+        n = len(a)
+        jackknife_values = numpy.fromiter((func(numpy.mean(a[range(0, i) + range(i+1, n)], dtype=dtype)) for i in range(n)), numpy.float)
+    else:
+        # Number of arguments for the function
+        k = a.shape[func_axis]
+        # Number of datapoints
+        n = a.size / k
+        jackknife_values = [func(*tuple(numpy.mean(numpy.reshape(numpy.take(a, [j,], axis=func_axis), -1)[range(0,i) + range(i+1, n)]) for j in range(k))) for i in range(n)]
 
     # Return the average value and the error of this averaged value
-    return numpy.mean(jackknife_values, axis=axis), math.sqrt(n - 1)*numpy.std(jackknife_values, axis=axis)
+    return numpy.mean(jackknife_values), math.sqrt(n - 1)*numpy.std(jackknife_values)
 
 
-def bootstrap(a, iterations, func=identity, axis=None, dtype=None):
+def bootstrap(a, iterations, func=identity, dtype=None):
     """
     Compute the bootstrap mean and error of an array with respect to a given function.
 
@@ -108,7 +147,7 @@ def bootstrap(a, iterations, func=identity, axis=None, dtype=None):
     return numpy.mean(bootstrap_values), math.sqrt(float(iterations)/float(iterations - 1))*numpy.std(bootstrap_values)
 
 
-def subsampling(a, samples, iterations, func=identity, axis=None, func_axis=None, dtype=None):
+def subsampling(a, samples, iterations, func=identity, func_axis=None, dtype=None):
     """
     Compute the subsampling mean and error of an array with respect to a given function.
 
